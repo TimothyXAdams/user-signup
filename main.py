1467fbaeb411160e2de681ef7d6deef7eb572db2
Timothy Adams <TimothyXAdams@gmail.com>
2017-07-04 16:41:08 -0700

    Project complete

--- main.py
+++ main.py
@@ -5,34 +5,42 @@ app = Flask(__name__)
 app.config['DEBUG'] = True      # displays runtime errors in the browser, too
 
 
-@app.route("/welcome", methods=['POST'])
-def welcome():
-    return ("Welcome, ", username)
+# @app.route("/welcome", methods=['POST'])
+# def welcome():
+#     return ("Welcome, ", username)
 
 
 
 @app.route("/chkform", methods=['POST'])
 def chkform():
-    error = "" #initialize error as empty string
+    username_error = "" #initialize error as empty string
+    password_error = ""
+    chkpwd_error   = ""
+    email_error    = ""
 
     username = request.form["username"]
     if len(username) <3 or len(username) > 20:
-    	error = "You must enter a valid user name"
+    	username_error = "You must enter a valid user name"
 
     password  =request.form["password"]
     if len(password) <3 or len(password) > 20:
-    	error = "Invalid password"
+    	password_error = "Invalid password"
 
     chkpwd = request.form["chkpwd"]
     if chkpwd != password:
-    	error = "Password mismatch"
+    	chkpwd_error = "Password mismatch"
 
     emailaddr = request.form["emailaddr"]
-    if "@" not in emailaddr or "." not in emailaddr:
-    	error = "Make sure email address is valid"
+    if emailaddr: #If it's not empty, since it's optional
+        if " " in emailaddr:
+            email_error = "Make sure email address is valid"
+        if len(emailaddr) < 3 or len(emailaddr) >20:
+            email_error = "Make sure email address is valid"
+        if "@" not in emailaddr or "." not in emailaddr:
+            email_error = "Make sure email address is valid"
 
-    if error:
-        return render_template("user_signup_form.html")
+    if username_error or password_error or chkpwd_error or email_error:
+        return render_template("user_signup_form.html", username_error=username_error, password_error=password_error, chkpwd_error=chkpwd_error, email_error=email_error, username=username, password=password, emailaddr=emailaddr)
     else:
         return render_template("welcome.html", username=username)
 
