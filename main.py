251637af839e7d9f16094164408ddb43b9f94614
Timothy Adams <TimothyXAdams@gmail.com>
2017-07-03 18:54:49 -0700

    added html form to base.html in TEMPLATES

--- main.py
+++ main.py
@@ -24,6 +24,7 @@ app.config['DEBUG'] = True      # displays runtime errors in the browser, too
 @app.route("/")
 def index():
     encoded_error = request.args.get("error")
-    return render_template('edit.html', watchlist=get_current_watchlist(), error=encoded_error and cgi.escape(encoded_error, quote=True))
+    return render_template('base.html')
+    #return render_template('edit.html', watchlist=get_current_watchlist(), error=encoded_error and cgi.escape(encoded_error, quote=True))
 
 app.run()
