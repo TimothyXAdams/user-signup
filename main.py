67999983dd0d54e5c5f909955d5b8494a4e7d059
Timothy Adams <TimothyXAdams@gmail.com>
2017-07-04 14:44:40 -0700

    refactored to not use check.html

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from flask import Flask, request, redirect, render_template
-import cgi
 
 app = Flask(__name__)
 
@@ -12,16 +11,30 @@ def welcome():
 
 
 
+@app.route("/chkform", methods=['POST'])
+def chkform():
+    error = "" #initialize error as empty string
 
-
-
+    if len(username) <3 or len(username) > 20:
+    	error = "You must enter a valid user name"
+    if len(password) <3 or len(password) > 20:
+    	error = "Invalid password"
+    if chkpwd != password:
+    	error = "Password mismatch"
+    if "@" not in emailaddr or "." not in emailaddr:
+    	error = "Make sure email address is valid"
+    if error:
+        return render_template("/", error=error)
+    else:
+        return render_template("/welcome")
 
 
 
 @app.route("/")
 def index():
     encoded_error = request.args.get("error")
-    return render_template('check.html', error=encoded_error and cgi.escape(encoded_error, quote=True))
+    error = ""
+    return render_template('user_signup_form.html', error=error)
     #return render_template('edit.html', watchlist=get_current_watchlist(), error=encoded_error and cgi.escape(encoded_error, quote=True))
 
 app.run()
